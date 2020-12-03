a37930d33483361b462bf009f0e292006c09b721
thedoodler <ramprakashsmail@gmail.com>
2020-12-03 15:57:30 +0530

    bug fix

--- 2d_vector.js
+++ 2d_vector.js
@@ -10,44 +10,44 @@ Vector2.prototype = {
   unit: function(){
     this.x = 1;
     this.y = 1;
-  }
+  },
 
   set: function(x,y){
     this.x = x;
     this.y = y;
-  }
+  },
 
   clone: function(){
     return new Vector2(this.x,this.y);
-  }
+  },
 
   add: function(new_vect){
     return new Vector2(this.x + new_vect.x, this.y + new_vect.y);
-  }
+  },
 
   subtract: function(new_vect){
     return new Vector2(this.x - new_vect.x, this.y - new_vect.y);
-  }
+  },
 
   scale: function(scalar){
     return new Vector2(this.x * scalar, this.y * scalar);
-  }
+  },
 
   dot: function(new_vect){
     return new Vector2(this.x * new_vect.x, this.y * new_vect.y);
-  }
+  },
 
   distance function(new_vect){
     return (((this.x - new_vect.x)**2) + ((this.y - new_vect.y)**2));
-  }
+  },
 
   magnitude: function(){
     return Math.sqrt(this.x**2 + this.y**2);
-  }
+  },
 
   magnitudesquare: function(){
     return this.x**2 + this.y**2;
-  }
+  },
 
   rotate: function(ang){
     cos = Math.cos(ang);
