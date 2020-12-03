f399aa3ea210629821c6f04067c91a28d1b197be
thedoodler <ramprakashsmail@gmail.com>
2020-12-03 15:31:12 +0530

    added clone add and subtract to vector 2

--- vector_funcs.js
+++ vector_funcs.js
@@ -1,1 +1,25 @@
 // this is a side helper class to perform vector calculations, that are useful for certain movement and calculations
+
+function Vector2(x,y){
+  this.x = (x === undefined) ? 0 : x;
+	this.y = (y === undefined) ? 0 : y;
+}
+
+Vector2.prototype = {
+  set: function(x,y){
+    this.x = x;
+    this.y = y;
+  }
+
+  clone: function(){
+    return new Vector2(this.x,this.y);
+  }
+
+  add: function(new_vect){
+    return new Vector2(this.x + new_vect.x, this.y + new_vect.y);
+  }
+
+  subtract: function(new_vect){
+    return new Vector2(this.x - new_vect.x, this.y - new_vect.y);
+  }
+}
